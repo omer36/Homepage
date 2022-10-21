e2ae39dcc13f9644e89609978757fff78d1d32a5
Omer Karim <116109574+omer36@users.noreply.github.com>
2022-10-21 15:59:04 +0300

    Delete watt2amp directory

--- watt2amp/script.js
+++
@@ -1,6 +0,0 @@
-function gorren(){
-    let wat = document.getElementById('w').value;
-    let volt =  document.getElementById('v').value;
-    re = wat/volt;
-    document.getElementById('result').innerHTML = re + " ئەمپێر";
-}
